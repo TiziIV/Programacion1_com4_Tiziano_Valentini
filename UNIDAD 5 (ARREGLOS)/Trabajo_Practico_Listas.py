8ebf7d27eabf1cb17210761b1e8c4e09f2a1ff80
TiziIV <valentini.tizianoignacio2@gmail.com>
2025-09-11 22:40:02 -0300

    Subo Trabajo Practico de Listas

---
+++ UNIDAD 5 (ARREGLOS)/Trabajo_Practico_Listas.py
@@ -0,0 +1,54 @@
+# EJERCICIO 1
+multiplos_de_4=list(range(4,101,4))
+print(multiplos_de_4)
+
+# EJERCICIO 2
+elementos=["Hamburguesa","RDR2","ANUEL","FUTBOL","FIESTA"]
+print(elementos[-2])
+
+# EJERCICIO 3
+lista_vacia=[]
+lista_vacia.append("HOLA")
+lista_vacia.append("SOY")
+lista_vacia.append("TIZIANO")
+print(lista_vacia)  
+
+# EJERCICIO 4
+animales=["perro","gato","conejo","pez"]
+animales[1]="loro"
+animales[-1]="oso"
+print(animales)
+
+# EJERCICIO 5
+# Lo que hace este programa es con MAX(buscar el valor mas grande)
+# y con .remove lo elimina. En definitiva el programa remueve el valor mas grande
+# de la lista
+
+# EJERCICIO 6
+Numeros_de_5_en_5=list(range(10,31,5))
+print(Numeros_de_5_en_5[0])
+print(Numeros_de_5_en_5[1])
+
+# EJERCICIO 7
+autos=["sedan","polo","suran","gol"]
+autos[1]="bora"
+autos[2]="scirocco"
+print(autos)
+
+# EJERCICIO 8
+dobles=[]
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print(dobles)
+
+# EJERCICIO 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1]="tallarines"
+compras[0].remove("pan")
+print(compras)
+
+# EJERCICIO 10
+lista_anidada=[15,True,[25.5,57.9,30.6],False]
+print(lista_anidada)
