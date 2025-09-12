17c855b410a68aadbcc3fe2cc0df3f42d2dfa764
TiziIV <valentini.tizianoignacio2@gmail.com>
2025-09-11 22:42:14 -0300

    Añado espacio entre Ejercicios para mejor legibilidad

--- UNIDAD 5 (ARREGLOS)/Trabajo_Practico_Listas.py
+++ UNIDAD 5 (ARREGLOS)/Trabajo_Practico_Listas.py
@@ -1,12 +1,15 @@
 # EJERCICIO 1
+print("")
 multiplos_de_4=list(range(4,101,4))
 print(multiplos_de_4)
 
 # EJERCICIO 2
+print("")
 elementos=["Hamburguesa","RDR2","ANUEL","FUTBOL","FIESTA"]
 print(elementos[-2])
 
 # EJERCICIO 3
+print("")
 lista_vacia=[]
 lista_vacia.append("HOLA")
 lista_vacia.append("SOY")
@@ -14,6 +17,7 @@ lista_vacia.append("TIZIANO")
 print(lista_vacia)  
 
 # EJERCICIO 4
+print("")
 animales=["perro","gato","conejo","pez"]
 animales[1]="loro"
 animales[-1]="oso"
@@ -25,17 +29,20 @@ print(animales)
 # de la lista
 
 # EJERCICIO 6
+print("")
 Numeros_de_5_en_5=list(range(10,31,5))
 print(Numeros_de_5_en_5[0])
 print(Numeros_de_5_en_5[1])
 
 # EJERCICIO 7
+print("")
 autos=["sedan","polo","suran","gol"]
 autos[1]="bora"
 autos[2]="scirocco"
 print(autos)
 
 # EJERCICIO 8
+print("")
 dobles=[]
 dobles.append(5*2)
 dobles.append(10*2)
@@ -43,6 +50,7 @@ dobles.append(15*2)
 print(dobles)
 
 # EJERCICIO 9
+print("")
 compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
 compras[2].append("jugo")
 compras[1][1]="tallarines"
@@ -50,5 +58,6 @@ compras[0].remove("pan")
 print(compras)
 
 # EJERCICIO 10
+print("")
 lista_anidada=[15,True,[25.5,57.9,30.6],False]
 print(lista_anidada)
